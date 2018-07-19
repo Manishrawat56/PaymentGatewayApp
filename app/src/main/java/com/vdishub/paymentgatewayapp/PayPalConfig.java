fd14e1c70677ce255cdce39f98241ad9846fbbb7
manishrawat56 <manish.rawat56@gmail.com>
2018-07-19 18:07:48 +0530

    Initial commit

---
+++ app/src/main/java/com/vdishub/paymentgatewayapp/PayPalConfig.java
@@ -0,0 +1,6 @@
+package com.vdishub.paymentgatewayapp;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AWXaYDDYKgB4RvGzQUDLWIOMJ3hYAL61yhknh9W_z6MQn2ju1-2gAiJM1By1d7AB_IoiqLaq0-vtHBR1";
+
+}
